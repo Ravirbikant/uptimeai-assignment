44e6a280010f963a04bf2c5ba9b7939633cf9ab4
Ravi Kant Jha <ravirbikant@gmail.com>
2026-02-14 17:05:43 +0530

    Starting with body

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from "./components/Navbar.jsx";
+import Body from "./components/Body.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Body />
     </>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import './Body.css';
+
+function Body() {
+  return (
+    <main className="body-main">
+      <div className="body-inner" />
+    </main>
+  );
+}
+
+export default Body;
